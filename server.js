f078d683e430b8b0356a6d98397da7975f9c0dae
Carly Moses <cmoses916@gmail.com>
2018-11-15 07:25:26 -0500

    Heroku

---
+++ server.js
@@ -0,0 +1,17 @@
+"use strict";
+const express = require("express");
+const app = express();
+// const bodyParser = require("body-parser");
+// const courses = require("./routes/courses");
+// const students = require("./routes/students.routes");
+
+app.use(bodyParser.json());
+// app.use("/portal", courses);
+// app.use("/portal", students);
+app.use(express.static(__dirname + "/public"));
+
+//must add line 14 when using heroku
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server listening on ${port}.`);
+});
