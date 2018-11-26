c6abd34c30decb3b6f2666a490e1c9295828e5c0
Carly Moses <cmoses916@gmail.com>
2018-11-26 10:56:46 -0500

    server.js edit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 
-app.use(json());
+// app.use(json());
 app.use(express.static(__dirname + "/public"));
 
 const port = process.env.PORT || 5000;
